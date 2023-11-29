ad90ac6ef2015158fc9728c6ab3fe2b317613df5
Steve10166 <109441555+Steve10166@users.noreply.github.com>
2023-11-28 20:20:49 -0500

    update knn

--- Data/KNN.py
+++ Data/KNN.py
@@ -2,31 +2,61 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.datasets import make_classification
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import precision_recall_curve, auc
+import matplotlib.pyplot as plt
 
+def plot_precision_recall_curve(model, X_test, y_test, label=None):
+    probs = model.predict_proba(X_test)[:, 1]
+    precision, recall, _ = precision_recall_curve(y_test, probs)
+    auc_prc = auc(recall, precision)
+    plt.plot(recall, precision, label=f'{label} (AUC-PRC = {auc_prc:.2f})')
+
+def plot_roc_curve(model, X_test, y_test, label=None):
+    probs = model.predict_proba(X_test)[:, 1]
+    fpr, tpr, _ = roc_curve(y_test, probs)
+    plt.plot(fpr, tpr, label=label)
 
 def main():
     X = pd.read_csv("x.csv")
     y = pd.read_csv("y.csv")
-    # Applying KNN
+
+    # Splitting the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    # Setting up KNN and the grid search
     # knn = KNeighborsClassifier()
     # param_grid = {'n_neighbors': [1, 3, 5, 7, 10, 15, 17, 19, 21]}
-
-    # grid_search = GridSearchCV(knn, param_grid, cv=5, scoring='roc_auc')
+    # grid_search = GridSearchCV(knn, param_grid, cv=5, scoring='average_precision')
     # grid_search.fit(X_train, y_train)
 
+    # # Best parameters from grid search
     # best_params = grid_search.best_params_
     # best_score = grid_search.best_score_
-    # print(best_params, best_score)
+    # print("Best Parameters:", best_params)
+    # print("Best Average Precision Score:", best_score)
 
-    knn = KNeighborsClassifier(n_neighbors=15)
-    knn.fit(X_train, y_train)
-    train_aucroc = roc_auc_score(y_train, knn.predict_proba(X_train)[:, 1])
-    test_aucroc = roc_auc_score(y_test, knn.predict_proba(X_test)[:, 1])
-    print(train_aucroc,test_aucroc) #0.9996789553714515 0.9519419136006353
+    # # Training KNN with the best parameters
+    # best_knn = KNeighborsClassifier(n_neighbors=best_params['n_neighbors'])
+    # best_knn.fit(X_train, y_train)
 
+    # # ROC AUC scores
+    # train_aucroc = roc_auc_score(y_train, best_knn.predict_proba(X_train)[:, 1])
+    # test_aucroc = roc_auc_score(y_test, best_knn.predict_proba(X_test)[:, 1])
+    # print("Train AUC-ROC:", train_aucroc)
+    # print("Test AUC-ROC:", test_aucroc)
+    plt.figure(figsize=(10, 8))
+    for n_neighbors in [1, 3, 5, 7, 10, 15, 17, 19, 21]:  # Selecting a subset of n_neighbors for demonstration
+        model = KNeighborsClassifier(n_neighbors=n_neighbors)
+        model.fit(X_train, y_train)
+        plot_precision_recall_curve(model, X_test, y_test, label=f'n_neighbors={n_neighbors}')
+    
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall Curves for KNN')
+    plt.legend(loc='lower left')
+    plt.show()
 if __name__ == "__main__":
     main()
--- Data/dt.py
+++ Data/dt.py
@@ -2,8 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+def plot_roc_curve(model, X_test, y_test, label=None):
+    probs = model.predict_proba(X_test)[:, 1]
+    fpr, tpr, _ = roc_curve(y_test, probs)
+    auc = roc_auc_score(y_test, probs)
+    plt.plot(fpr, tpr, label=f'{label} (AUC = {auc:.2f})')
 
 def main():
     X = pd.read_csv("x.csv")
@@ -17,28 +23,50 @@ def main():
 
     # Defining the parameter grid for Decision Tree
     param_grid = {
-        'max_depth': [None, 10, 20, 30, 40, 50],
-        'min_samples_leaf': [1, 2, 4, 6, 8, 10],
+        'max_depth': [None, 10],
+        'min_samples_leaf': [1, 8],
         'criterion': ['gini', 'entropy']
     }
 
     # Grid Search with Cross-Validation
     grid_search = GridSearchCV(dt, param_grid, cv=5, scoring='roc_auc')
-    grid_search.fit(X_train, y_train)
+    # grid_search.fit(X_train, y_train)
 
-    # Best parameters and score
-    best_params = grid_search.best_params_
-    best_score = grid_search.best_score_
-    print("Best Parameters:", best_params)
-    print("Best Score:", best_score)
+    # # Best parameters and score
+    # best_params = grid_search.best_params_
+    # best_score = grid_search.best_score_
+    # print("Best Parameters:", best_params)
+    # print("Best Score:", best_score)
+
+
 
     # Train and evaluate the model with best parameters
-    dt_best = DecisionTreeClassifier(**best_params)
+    dt_best = DecisionTreeClassifier(criterion='gini', max_depth=10, min_samples_leaf=8)
     dt_best.fit(X_train, y_train)
     train_aucroc = roc_auc_score(y_train, dt_best.predict_proba(X_train)[:, 1])
     test_aucroc = roc_auc_score(y_test, dt_best.predict_proba(X_test)[:, 1])
     print("Train AUC-ROC:", train_aucroc)
-    print("Test AUC-ROC:", test_aucroc)
+    print("Test AUC-ROC:", test_aucroc) 
+    #Best Parameters: {'criterion': 'entropy', 'max_depth': 10, 'min_samples_leaf': 8}
+    # Best Score: 0.9210252246537729
+    # Train AUC-ROC: 0.9957812660910474
+    # Test AUC-ROC: 0.9232914994218115
+
+   
+    # grid_search = GridSearchCV(dt, param_grid, cv=5, scoring='roc_auc')
+    # grid_search.fit(X_train, y_train)
+    # plt.figure(figsize=(10, 8))
+    # for params in grid_search.cv_results_['params'][:10]:  # Selecting first 5 combinations for demonstration
+    #     model = DecisionTreeClassifier(**params)
+    #     model.fit(X_train, y_train)
+    #     plot_roc_curve(model, X_test, y_test, label=f'{params}')
+    
+    # plt.plot([0, 1], [0, 1], linestyle='--', color='r', label='No Skill')
+    # plt.xlabel('False Positive Rate')
+    # plt.ylabel('True Positive Rate')
+    # plt.title('Receiver Operating Characteristic (ROC) Curves')
+    # plt.legend(loc='lower right')
+    # plt.show()
 
 if __name__ == "__main__":
     main()
